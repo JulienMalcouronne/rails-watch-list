f124c67667de60e02d81ca9d7d99ab91f9715f65
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-11-11 17:51:17 +0100

    ok

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,23 @@
+class MoviesController < ApplicationController
+     before_action :set_movie, only: %i[show]
+  def index
+    @movies = Movie.all
+  end
+
+  def show; end
+
+
+  def search
+     @movies = Movie.where("name LIKE '%#{params[:query]}%'")
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+puts "start seeding"
+
+  url = "http://tmdb.lewagon.com/movie/top_rated"
+  url_serialized = URI.open(url).read
+  top = JSON.parse(url_serialized)
+  all_movies = top["results"]
+
+  all_movies.each do |movie|
+    params = {
+      title: movie["title"],
+      overview: movie["overview"],
+      rating: movie["vote_average"],
+      poster_url: "https://www.themoviedb.org/t/p/w1280#{movie["poster_path"]}"
+    }
+  Movie.create!(params)
+  end
+
+
+puts "seeding end..."
